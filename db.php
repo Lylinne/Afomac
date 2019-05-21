f52048d2e0dc00ab426d0ab89e9bd779387955eb
Lylinne <nikkinoaya@gmail.com>
2019-05-21 14:02:35 +0200

    first

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
-require 'config.php';
+require ('config.php');
 
 
 /* Connexion à une base MySQL avec l'invocation de pilote */
@@ -9,5 +9,5 @@ $dsn = 'mysql:dbname='.$dbname.';host='.$dbhost.';charset=UTF8';
 try {
     $pdo = new PDO($dsn, $dbuser, $dbpassword);
 } catch (PDOException $e) {
-    echo 'Connexion échouée : ' . $e->getMessage();
+    echo 'Connexion échouée : ' . $e->getMessage("Erreur de connexion SQL");
 }
